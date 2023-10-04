e82cb301f7ba93a406a3e0a08d3cf87bd2785084
flooijdt <flooijdt@posteo.de>
2023-10-04 10:57:03 -0300

    default commit.

--- main.cpp
+++ main.cpp
@@ -5,6 +5,22 @@
 
 int main(int argc, char *argv[])
 {
+    int height = 0;
+
+    std::cout << "what is the ceiling height?: ";
+    std::cin >> height;
+
+    int total_lenght = 0;
+    
+    do {
+        int lenght = 0;
+        std::cout << "\nwhat is the wall lenght?: ";
+        std::cin >> lenght;
+
+        total_lenght = ++lenght;
+    }while (total_lenght > 0);
+
+    std::cout << "\nthe total area is " << height * total_lenght << "\n";
 
     return 0;
 }
