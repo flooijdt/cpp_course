df05468f339df1a60c826a04f26e465e67273a83
flooijdt <flooijdt@posteo.de>
2023-10-04 10:34:17 -0300

    default commit.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,10 @@
-#include <QCoreApplication>
-#include <QDebug>
-#include<string>
+// #include <QCoreApplication>
+// #include <QDebug>
+// #include<string>
 #include<iostream>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
 
-    QString task = "Please do the dishes!";
-
-    int max = 0;
-
-    std::cout << "give me the ammount of times you want me to repeat: \n";
-    std::cin >> max;
-
-    for (int var = 0; var < 50; ++var) {
-        qInfo() << var;
-        qInfo() << task;
-        if (var == max) {
-            break;
-        }
-    }
-
-    return a.exec();
+    return 0;
 }
