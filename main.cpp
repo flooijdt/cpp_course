5ebec730284d73e8dda4decf422150997abb23b8
flooijdt <flooijdt@posteo.de>
2023-10-04 09:57:36 -0300

    default commit.

--- main.cpp
+++ main.cpp
@@ -9,7 +9,18 @@ int main(int argc, char *argv[])
 
     QString task = "Please do the dishes!";
 
-   qInfo() << task;
+    int max = 0;
+
+    std::cout << "give me the ammount of times you want me to repeat: \n";
+    std::cin >> max;
+
+    for (int var = 0; var < 50; ++var) {
+        qInfo() << var;
+        qInfo() << task;
+        if (var == max) {
+            break;
+        }
+    }
 
     return a.exec();
 }
